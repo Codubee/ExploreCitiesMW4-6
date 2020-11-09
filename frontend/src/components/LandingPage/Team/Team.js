d550614cac1a08a019eb663bf94b7a7e8d8d94c3
Imaad Aziz <imaadaziz@gmail.com>
2020-11-08 19:17:54 -0600

    Added Basic Team Component

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -12,10 +12,9 @@ import {
         <Card>
           <CardImg/>
           <CardBody>
-            <CardTitle tag="h1">Card title</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-            <Button>Button</Button>
+            <CardTitle tag="h1">Team</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Subtitle</CardSubtitle>
+            <CardText>Member Description</CardText>
           </CardBody>
         </Card>
       </div>
