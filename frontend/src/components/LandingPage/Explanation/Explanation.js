7d0060cd21189b4d5d9cf2f360e34d57a3d81c6b
olaade97 <57572771+olaade97@users.noreply.github.com>
2020-11-11 15:37:39 -0600

    added styling and corrected the description

--- frontend/src/components/LandingPage/Explanation/Explanation.js
+++ frontend/src/components/LandingPage/Explanation/Explanation.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, CardLink } from 'reactstrap';
+import './Explanation.css';
 
 function Explanation(){
  
   return (
-    <div>
+    <div className ="page">
       <Jumbotron>
-        <h1 className="display-3">Explanation</h1>
-        <p className="lead">The concept of this application is to create a application that houses a variety of recipes for the user </p>
+        <h1 className="display-3" >Explanation</h1>
+        <p className="lead">The concept of this application is to create a application that allows user to explore cities within a radius of their choosing   </p>
         <hr className="my-2" />
         <p>The general purpose of this application as follows: </p>
-        <p>Suppose you have ingridents in your in your home but don't know what to make with them. That's where our application comes in handy. The user will enter the ingridents into the application and the application will display a list of recipes that can be made with just those ingridents. </p>
-
+        <dl>
+          <dt>Suppose you wanted to take a trip and you didn't know where to go or what to do. With our application, you'd be able to enter the zip code of where you'd like to go and what will be returned will be the following: </dt>
+             <dd> -Cities in and around that area</dd> 
+             <dd> -Yelp results regarding that area</dd>
+             <dd> -Flights data regarding that area</dd>
+             <dd> -Uber results regarding that area</dd>
+             <dd> -Eventbrite results regarding that area</dd> 
+        </dl>
         <p className="lead">
           <Button color="primary">Learn More</Button>
         </p>
