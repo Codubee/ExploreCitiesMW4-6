6210b30bae1db105448339cadbe099cf25bc043d
Salmaan Afzal <salmaan@Salmaans-MBP.fios-router.home>
2020-11-12 00:49:08 -0600

    changes to formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage'
-import Navbar from './Navbar'
+
 function App() {
-  return (<div>
-    <Navbar/>
+  return (
+    
     <LandingPage/>
-    </div>
+    
   );
 }
 
--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
 //Import components that you create here
 
 function LandingPage(){
     return(
         //Use components that you import here
-        <></>
+        <>
+        <Navbar/>
+        </>
     )
 }
 
