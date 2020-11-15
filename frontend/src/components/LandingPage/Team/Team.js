6a95fa041b208491cc664747ca85a54f69746d5f
Imaad Aziz <imaadaziz@gmail.com>
2020-11-15 17:34:47 -0600

    Created a basic Teams component with all members

--- frontend/src/components/LandingPage/Team/Team.js
+++ frontend/src/components/LandingPage/Team/Team.js
@@ -1,30 +1,69 @@
 import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, CardHeader
+    CardTitle, CardHeader, CardLink
   } from 'reactstrap';
   import './Team.css'
 
   const Team = (props) => {
     return (
-        <div>
-        <CardHeader tag='h1'>Team</CardHeader>
-        <Card>
-          <CardImg/>
-          <CardBody>
-            <CardTitle tag="h1">Team</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Subtitle</CardSubtitle>
-            <CardText>Member Description</CardText>
-          </CardBody>
-        </Card>
+      <div class="Team">
+        <CardHeader tag='h1'>Team Members</CardHeader>
+        <TeamCard 
+          class="TeamCard"
+          memberName="Imaad Aziz"
+          imageSource="profpic.jpg"
+          memberDescription="Imaad is a sophomore at the University of Texas at Dalllas majoring in Computer Science with a minor in business analytics. When he isn't developing websites he enjoys watching basketball and playing video games. His github 
+          can be found below."
+          link='https://github.com/ImaadAziz'
+          />
+          <TeamCard 
+          class="TeamCard"
+          memberName="Jaden Dick"
+          memberDescription="Jaden is a junior at the University of Texas at Dallas majoring in Computer Science. In his free time, Jaden enjoys cooking and playing both basketball and tennis. His future projects and repositories can be
+          found in the link below."
+          link='https://github.com/jadendick'
+
+          />
+           <TeamCard 
+          class="TeamCard"
+          memberName="Anjelica Avorque"
+          memberDescription="Anjelica is a junior at the University of Texas at Dallas majoring in Computer Science. In her free time she enjoys drawing, anime, video games, and exploring new cafes! Her other projects can be found at her
+          Github in the link below."
+          link='https://github.com/evanjelica'
+
+          />
+           <TeamCard 
+          class="TeamCard"
+          memberName="Olaoluwa Babatunde"
+          memberDescription="Ola is a senior at the University of Texas at Dallas majoring in Computer Science. In his downtime he enjoys playing tennis, playing volleyball, and keeping up with world news. Ola's other work and future projects can be 
+          found at his Github in the link below."
+          link='https://github.com/olaade97'
+
+          />
+           <TeamCard 
+          class="TeamCard"
+          memberName="Salmaan Afzal"
+          memberDescription="Salmaan is currently a sophomore at the University of Texas at Dallas majoring in Computer Science. In his free time he enjoys playing video games and mathematics. Salmaan's future projects can be found at the
+          Github link below."
+          link='https://github.com/Salmaan21'
+
+          />
+          
       </div>
     );
 };
 
 const TeamCard = (props) => {
     return (
-        <Card>
-            
+        <Card class='Card' style={{backgroundColor:"#eee", borderColor:"#333", margin:".5%"}}>
+          <CardImg src={props.imageSource}></CardImg>
+          <CardBody>
+            <CardTitle tag='h2'>{props.memberName}</CardTitle>
+
+            <CardText class='text'>{props.memberDescription}</CardText>
+            <CardLink class='link' href={props.link}>{props.link}</CardLink>
+          </CardBody>
         </Card>
     );
 }
