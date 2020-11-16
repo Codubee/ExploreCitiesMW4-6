8600c214d12cce815b808617588a8da1ca90dca6
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2020-11-16 15:34:34 -0600

    Made changes to the features component

--- frontend/src/components/LandingPage/Features/Features.js
+++ frontend/src/components/LandingPage/Features/Features.js
@@ -1,7 +1,60 @@
 import React from 'react'
+import {
+        Card, CardImg, CardText, CardBody,
+        CardTitle, Button
+        } from 'reactstrap'
+import './Features.css'
 
 function Features()
 {
+    return
+    (
+        <div className = "featuresPage">
+            <h1 id = "header">App Features</h1>
+        <div className = "features">
 
+            <featuresCard
+                CardImg = "Stock photo"
+                CardTitle = "Explore nearby cities!"
+                CardText = "Description"
+                Button = "Click here"
+            />
+            <featuresCard
+                CardImg = "Stock photo"
+                CardTitle = "Enjoy the local cuisine!"
+                CardText = "Description"
+                Button = "Click here"
+            />
+            <featuresCard
+                CardImg = "Stock photo"
+                CardTitle = "Find cool things to do!"
+                CardText = "Description"
+                Button = "Click here"
+            />
+        </div>
+    </div>
+    );
+}
+
+const featuresCard = (props) =>
+{
+    return
+    (
+        <Card>
+            <CardImg top width = "100%" src ={props.CardImg}>
+                <CardBody>
+                    <CardTitle tag="h5">
+                        {props.CardTitle}
+                    </CardTitle>
+                    <CardText>
+                        {props.CardText}
+                    </CardText>
+                    <Button>
+                        {props.Button}
+                    </Button>
+                </CardBody>
+            </CardImg>
+        </Card>
+    );
 }
 export default Features;
