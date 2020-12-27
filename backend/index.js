74357d5ffa9a7770e6a9c4f303541bb5d5c5a173
Salmaan21 <57534775+Salmaan21@users.noreply.github.com>
2020-12-27 14:24:37 -0600

    Cb 13 weather (#10)
    * The Navbar for Explore Cities
    
    * changes to formatting
    
    * folder change
    
    * landing
    
    * weather api
    
    * weather api
    
    * weather api
    
    * weather api
    
    * weather api
    
    Co-authored-by: Salmaan Afzal <salmaan@Salmaans-MBP.fios-router.home>
    Co-authored-by: Salmaan Afzal <salmaan@Salmaans-MacBook-Pro.local>

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,21 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+app.get('/weatherbit/:zip',(req,res)=>{
+    console.log(req.params.zip)
+    let zip = req.params.zip
+    const api_key = process.env.WEATHER_API_KEY
+    axios.get("https://api.weatherbit.io/v2.0/current?postal_code="+zip +"&units=I&key="+api_key)
+    .then((response)=>{
+        console.log(response)
+        res.status(200).json(response.data)
+    })
+    .catch((error)=>{
+        console.log(error)
+        res.status(400).json({error:"Error Occurred"})
+    })
+})
+
 app.get('/yelpGetThingsToDo',(req,res)=>{
 
     let zip = req.query.zip
@@ -29,5 +44,4 @@ app.get('/yelpGetThingsToDo',(req,res)=>{
 app.listen(port,()=>{
     console.log('API is up and running')
 })
-
 module.exports = app;
