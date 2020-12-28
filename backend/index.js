4628a31921974e23ed29df02ac75094005645f4a
olaade97 <57572771+olaade97@users.noreply.github.com>
2020-12-28 16:17:57 -0600

    [CB-11] Gets restaurant data associated with a zip code (#9)
    * grabs data associated with a zipcode
    
    * changed file name
    
    * changed file name restaurant -> index.js
    
    * Removed whitespace
    
    Co-authored-by: Miguel <info@codubee.com>

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,22 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+app.get('/getRestaurant/:zip',(req,res)=>{
+    console.log(req.params.zip)
+    let zip = req.params.zip
+    
+    axios.get("https://api.yelp.com/v3/businesses/search?term=restaurants&location="+zip,
+        {headers:{authorization:process.env.YELP_KEY}})
+    .then((response)=>{
+        console.log(response.data)
+        res.status(200).json(response.data)
+    })
+    .catch((error)=>{
+        console.log(error)
+        res.status(400).json({error:"Error Occurred"})
+    })
+})
+
 app.get('/weatherbit/:zip',(req,res)=>{
     console.log(req.params.zip)
     let zip = req.params.zip
