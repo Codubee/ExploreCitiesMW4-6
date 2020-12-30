eebcad76203b0150fa233cf25365a838c6e6ff75
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2020-12-29 22:42:57 -0600

    Added routing component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+
 import LandingPage from './components/LandingPage/LandingPage'
 import NavBar from './components/NavBar/Navbar'
+import Explanation from './components/LandingPage/Explanation/Explanation';
+import Features from './components/LandingPage/Features/Features';
+import Team from './components/LandingPage/Team/Team';
+import Tools from './components/LandingPage/Tools/Tools';
 
-function App() {
+import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+export default function App() {
   return (
-    <div>
-      <NavBar />
-      <LandingPage />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <NavBar />
+            <li>
+              <Link to ="/">Home</Link>
+            </li>
+            <li>
+              <Link to = "/explanation">Explanation</Link>
+            </li>
+            <li>
+              <Link to = "/features">Features</Link>
+            </li>
+            <li>
+              <Link to = "/team">Team</Link>
+            </li>
+            <li>
+              <Link to = "/tools">Tools</Link>
+            </li>
+            
+          </ul>
+        </nav>
+        
+        <Switch>
+          <Route exact path ="/">
+            <Home />
+            <h3>Welcome to Explore Cities!</h3>
+          </Route>
+          <Route path ="/explanation">
+            <Explanation />
+          </Route>
+          <Route path ="/features">
+            <Features />
+          </Route>
+          <Route path ="/team">
+            <Team />
+          </Route>
+          <Route path = "/tools">
+            <Tools />
+          </Route>
 
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home(){
+  return <h2>Home</h2>
+}
